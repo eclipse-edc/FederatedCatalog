3a3c532f2dbf3777ac92f55f3fb10389b9a8949a
Benjamin SCHOLTES <benjamin.scholtes@amadeus.com>
2024-02-29 09:34:15 +0100

    docs: fix catalog swagger API

--- extensions/api/federated-catalog-api/src/main/java/org/eclipse/edc/catalog/api/query/FederatedCatalogApi.java
+++ extensions/api/federated-catalog-api/src/main/java/org/eclipse/edc/catalog/api/query/FederatedCatalogApi.java
@@ -16,6 +16,7 @@ package org.eclipse.edc.catalog.api.query;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -25,14 +26,16 @@ import jakarta.json.JsonArray;
 import org.eclipse.edc.catalog.spi.model.FederatedCatalogCacheQuery;
 import org.eclipse.edc.spi.types.domain.offer.ContractOffer;
 
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        info = @Info(description = "This represents the Federated Catalog API. It serves the cached Catalogs fetched from the data providers.",
+                title = "Federated Catalog API", version = "1"))
 @Tag(name = "Federated Catalog")
 public interface FederatedCatalogApi {
-    @Operation(description = "Obtains all contract offers currently held by this cache instance",
+    @Operation(description = "Obtains all Catalog currently held by this cache instance",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "A list of contract offers is returned, potentially empty",
+                    @ApiResponse(responseCode = "200", description = "A list of Catalog is returned, potentially empty",
                             content = @Content(array = @ArraySchema(schema = @Schema(implementation = ContractOffer.class)))),
-                    @ApiResponse(responseCode = "50x", description = "A Query could not be completed due to an internal error")
+                    @ApiResponse(responseCode = "500", description = "A Query could not be completed due to an internal error")
             }
 
     )
