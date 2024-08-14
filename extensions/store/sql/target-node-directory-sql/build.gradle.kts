e20b1f1e52fbdb0a504e89c41427848c739225d8
Benjamin SCHOLTES <benjamin.scholtes@amadeus.com>
2024-08-14 14:49:51 +0200

    feat: sql implementation of target node directory

---
+++ extensions/store/sql/target-node-directory-sql/build.gradle.kts
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (c) 2024 Amadeus IT Group
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Amadeus IT Group - initial API and implementation
+ *
+ */
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":spi:crawler-spi"))
+    implementation(libs.edc.sql.core) // for the SqlStatements
+    implementation(libs.edc.sql.bootstrapper)
+    implementation(libs.edc.spi.transaction.datasource)
+    implementation(libs.edc.lib.util)
+
+    testImplementation(libs.edc.junit)
+    testImplementation(testFixtures(libs.edc.sql.core))
+    testImplementation(testFixtures(project(":spi:crawler-spi")))
+}
