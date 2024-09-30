173d0f396c0a509b39f98f0659c9882d5bd25d36
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-09-30 09:24:59 +0200

    fix: avoid exception DSP Request Action (#252)
    * fix: avoid exception DSP Request Action
    
    * DEPENDENCIES

--- core/federated-catalog-core/src/test/java/org/eclipse/edc/catalog/query/PagingCatalogFetcherTest.java
+++ core/federated-catalog-core/src/test/java/org/eclipse/edc/catalog/query/PagingCatalogFetcherTest.java
@@ -16,30 +16,13 @@ package org.eclipse.edc.catalog.query;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import org.eclipse.edc.catalog.cache.query.PagingCatalogFetcher;
 import org.eclipse.edc.catalog.spi.CatalogConstants;
-import org.eclipse.edc.catalog.transform.JsonObjectToCatalogTransformer;
-import org.eclipse.edc.catalog.transform.JsonObjectToDataServiceTransformer;
-import org.eclipse.edc.catalog.transform.JsonObjectToDatasetTransformer;
-import org.eclipse.edc.catalog.transform.JsonObjectToDistributionTransformer;
 import org.eclipse.edc.connector.controlplane.catalog.spi.Catalog;
 import org.eclipse.edc.connector.controlplane.catalog.spi.CatalogRequestMessage;
-import org.eclipse.edc.connector.controlplane.catalog.spi.DataService;
-import org.eclipse.edc.connector.controlplane.catalog.spi.Dataset;
-import org.eclipse.edc.connector.controlplane.catalog.spi.Distribution;
-import org.eclipse.edc.connector.controlplane.transform.odrl.from.JsonObjectFromPolicyTransformer;
-import org.eclipse.edc.connector.controlplane.transform.odrl.to.JsonObjectToPolicyTransformer;
-import org.eclipse.edc.connector.core.agent.NoOpParticipantIdMapper;
 import org.eclipse.edc.jsonld.TitaniumJsonLd;
-import org.eclipse.edc.jsonld.util.JacksonJsonLd;
 import org.eclipse.edc.junit.annotations.ComponentTest;
-import org.eclipse.edc.policy.model.Policy;
-import org.eclipse.edc.protocol.dsp.catalog.transform.from.JsonObjectFromCatalogTransformer;
-import org.eclipse.edc.protocol.dsp.catalog.transform.from.JsonObjectFromDataServiceTransformer;
-import org.eclipse.edc.protocol.dsp.catalog.transform.from.JsonObjectFromDatasetTransformer;
-import org.eclipse.edc.protocol.dsp.catalog.transform.from.JsonObjectFromDistributionTransformer;
 import org.eclipse.edc.spi.message.Range;
 import org.eclipse.edc.spi.message.RemoteMessageDispatcherRegistry;
 import org.eclipse.edc.spi.response.StatusResult;
@@ -48,14 +31,11 @@ import org.eclipse.edc.transform.spi.TypeTransformerRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.edc.catalog.test.TestUtil.createCatalog;
+import static org.eclipse.edc.catalog.test.TestUtil.registerTransformers;
 import static org.eclipse.edc.jsonld.util.JacksonJsonLd.createObjectMapper;
 import static org.mockito.ArgumentCaptor.forClass;
 import static org.mockito.ArgumentMatchers.any;
@@ -76,7 +56,7 @@ class PagingCatalogFetcherTest {
 
     @BeforeEach
     void setup() {
-        registerTransformers();
+        registerTransformers(typeTransformerRegistry);
 
         fetcher = new PagingCatalogFetcher(dispatcherRegistryMock, mock(), objectMapper, typeTransformerRegistry, jsonLdService);
     }
@@ -109,13 +89,6 @@ class PagingCatalogFetcherTest {
                 .containsExactly(new Range(0, 5), new Range(5, 10), new Range(10, 15));
     }
 
-    public Dataset createDataset(String id) {
-        return Dataset.Builder.newInstance()
-                .offer("test-offer", Policy.Builder.newInstance().build())
-                .distribution(Distribution.Builder.newInstance().format("test-format").dataService(DataService.Builder.newInstance().build()).build())
-                .id(id)
-                .build();
-    }
 
     private StatusResult<byte[]> toBytes(Catalog catalog) throws JsonProcessingException {
         var jo = typeTransformerRegistry.transform(catalog, JsonObject.class).getContent();
@@ -133,32 +106,6 @@ class PagingCatalogFetcherTest {
 
     private Catalog emptyCatalog() {
         return Catalog.Builder.newInstance().id("id").participantId("test-participant").datasets(emptyList()).dataServices(emptyList()).build();
-    }
-
-    private Catalog createCatalog(int howManyOffers) {
-        var datasets = IntStream.range(0, howManyOffers)
-                .mapToObj(i -> createDataset("dataset-" + i))
-                .collect(Collectors.toList());
-
-        var build = List.of(DataService.Builder.newInstance().build());
-        return Catalog.Builder.newInstance().participantId("test-participant").id("catalog").datasets(datasets).dataServices(build).build();
-    }
-
-    // registers all the necessary transformers to avoid duplicating their behaviour in mocks
-    private void registerTransformers() {
-        var factory = Json.createBuilderFactory(Map.of());
-        var mapper = JacksonJsonLd.createObjectMapper();
-        var participantIdMapper = new NoOpParticipantIdMapper();
-        typeTransformerRegistry.register(new JsonObjectFromCatalogTransformer(factory, mapper, participantIdMapper));
-        typeTransformerRegistry.register(new JsonObjectFromDatasetTransformer(factory, mapper));
-        typeTransformerRegistry.register(new JsonObjectFromDataServiceTransformer(factory));
-        typeTransformerRegistry.register(new JsonObjectFromPolicyTransformer(factory, participantIdMapper));
-        typeTransformerRegistry.register(new JsonObjectFromDistributionTransformer(factory));
-        typeTransformerRegistry.register(new JsonObjectToCatalogTransformer());
-        typeTransformerRegistry.register(new JsonObjectToDatasetTransformer());
-        typeTransformerRegistry.register(new JsonObjectToDataServiceTransformer());
-        typeTransformerRegistry.register(new JsonObjectToPolicyTransformer(participantIdMapper));
-        typeTransformerRegistry.register(new JsonObjectToDistributionTransformer());
     }
 
 }
