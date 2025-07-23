32a551d1c7a995e13aa327716f77cf93db784b88
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2025-07-23 11:42:07 +0200

    build: use edc-build from Gradle Plugin Portal (#335)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@
 
 plugins {
     `java-library`
+    alias(libs.plugins.edc.build)
 }
 
 val fccScmConnection: String by project
@@ -23,12 +24,15 @@ val fccScmUrl: String by project
 buildscript {
     dependencies {
         val version: String by project
-        classpath("org.eclipse.edc.edc-build:org.eclipse.edc.edc-build.gradle.plugin:$version")
+        classpath("org.eclipse.edc.autodoc:org.eclipse.edc.autodoc.gradle.plugin:$version")
     }
 }
 
+val edcBuildId = libs.plugins.edc.build.get().pluginId
+
 allprojects {
-    apply(plugin = "${group}.edc-build")
+    apply(plugin = edcBuildId)
+    apply(plugin = "org.eclipse.edc.autodoc")
 
     configure<org.eclipse.edc.plugins.edcbuild.extensions.BuildExtension> {
         pom {
